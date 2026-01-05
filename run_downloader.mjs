60e03f323a9f72002192e6775b05cd28511f60c9
Michael Polom <michaelpolom@gmail.com>
2026-01-05 20:16:29 +0000

    updated run script

--- run_downloader.mjs
+++ run_downloader.mjs
@@ -3,3 +3,8 @@ import('./downloader/index.js')
     console.error(err);
     process.exit(1);
   });
+  import('./scraper/scrape.js')
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
